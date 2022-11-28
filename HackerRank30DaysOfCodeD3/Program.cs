91ece8b018e8631ccf91ec959c10c05fbf8d2e25
Özgür <zgr@DESKTOP-1NLBACI>
2022-11-29 01:45:52 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Bizden istediği şey n tekse ve 6 ila 20 arasındaysa ekrana weird yazsın. Değilse Not Weird yazsın.
+
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+
+            if (N % 2 == 1 || (N >= 6 && N <= 20))
+            {
+                Console.WriteLine("Weird");
+            }
+            else
+            {
+                Console.WriteLine("Not Weird");
+            }
+        }
+    }
+}
